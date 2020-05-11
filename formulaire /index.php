083c1a5ca75a44b2e9af4b88e302cf625fbaff31
AntoineAssanvo <62053287+AntoineAssanvo@users.noreply.github.com>
2020-05-11 13:08:41 +0200

    structure

---
+++ formulaire /index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>FORMULAIRE</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="contact-form">
+      <form method="POST">
+        <input type="text" name='contact_name' placeholder="Comment t'appelles-tu ?" class="input" />
+        <input type="text" name='age' placeholder="quel est ton age ???" class="input" />
+        <input type="email" name='mail' placeholder="Donne nous ton email" class="input">
+
+        <select name="profession" class="input">
+          <option value="secondaire" class="option">Etudiant(e) dans le secondaire</option>
+          <option value="superieur" class="option">Etudiant(e) dans le superieur</option>
+          <option value="salarié" class="option">Salarié"</option>
+          <option value="non-salarié" class="option">Non-salarié"</option>
+        </select>
+
+        <input type="text" name='subject' placeholder=" le sujet de ton message est ?" class="input" />
+
+
+        <textarea name="message" cols="50" rows="5" placeholder="Ecrire nous dans cette zone !" class="msg"></textarea>
+        <!-- je met le bouton envoie-->
+        <input type="submit" value="Envoyer">
+
+      </form>
+    </div>
+  </div>
+</body>
+
+</html>
+
+<?php
+//je me connecet depuis la base//
+//include('lie/connect.php');
+ini_set('display_errors', 'on');
+$bdd = new PDO('mysql:host=localhost;dbname=spacejob', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+// je m'informe si il y'a une erreur
+
+
+// je met une condition//
+if (isset($_POST['contact_name']) and isset($_POST['age']) and  isset($_POST['mail']) and  isset($_POST['profession']) and  isset($_POST['subject']) and  isset($_POST['message'])) {
+
+  // je prepare la requete à envoyer //
+  $requete = $bdd->prepare("INSERT INTO  spacejob_form (contact_name, age, mail, profession, subject, message) VALUES (?, ?, ?, ?, ?, ?)");
+  //j'execute la requete//
+  $requete->execute(array($_POST['contact_name'], $_POST['age'], $_POST['mail'], $_POST['profession'], $_POST['subject'], $_POST['message']));
+}
---
+++ formulaire /lie/connect.php
@@ -0,0 +1,2 @@
+<?php
+$bdd = new PDO ('mysql:host=localhost;dbname=spacejob', 'root', 'root',array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
