f3889feb8ba900e134c8d3f3a2f872b9adc449cd
AntoineAssanvo <62053287+AntoineAssanvo@users.noreply.github.com>
2020-06-06 22:41:48 +0200

    modif

---
+++ confirmation.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    < <meta charset="UTF-8">
+        <title>Space Job</title>
+        <link rel="icon" type="image/png" href="images/icon.png" />
+        <link rel="stylesheet" href="./redirection/style_c.css">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <title>Envoie validé</title>
+</head>
+
+<body>
+    <h1>Merci de nous avoir envoyé un message </h1>
+    <a href="./accueil.php"> Retourner à l'accueil</a>
+</body>
+
+</html>
