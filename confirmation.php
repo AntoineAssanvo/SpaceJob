c46fffd090642d3bfe7bfe9406b1c31e76d63e24
AntoineAssanvo <62053287+AntoineAssanvo@users.noreply.github.com>
2020-06-07 11:15:24 +0200

    modif des page html en php et changement des liens ancres

--- confirmation.php
+++ confirmation.php
@@ -3,7 +3,7 @@
 
 <head>
     < <meta charset="UTF-8">
-        <title>Space Job</title>
+        <title>🚀 Envoie confirmé</title>
         <link rel="icon" type="image/png" href="images/icon.png" />
         <link rel="stylesheet" href="./redirection/style_c.css">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap" rel="stylesheet">
@@ -13,7 +13,7 @@
 
 <body>
     <h1>Merci de nous avoir envoyé un message </h1>
-    <a href="./accueil.php"> Retourner à l'accueil</a>
+    <a href="./accueil.php"> 🚀 sur la page l'accueil</a>
     <img src=" ./images/icon.png" alt="">
 </body>
 
