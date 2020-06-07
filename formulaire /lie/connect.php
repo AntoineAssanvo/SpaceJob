adb5c39c23a3d0372b37afbcc4d37f7971af2fa4
AntoineAssanvo <62053287+AntoineAssanvo@users.noreply.github.com>
2020-06-07 19:59:30 +0200

    Update connect.php

--- formulaire /lie/connect.php
+++ formulaire /lie/connect.php
@@ -1,2 +1,3 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=spacejob', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+//$bdd = new PDO('mysql:host=localhost;dbname=spacejob', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$bdd = new PDO('mysql:host=sqletud.u-pem.fr;dbname=aassanvo_db', 'aassanvo', 'antoineassvx', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
