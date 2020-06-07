ec706f4517d2aa3c79f1241556afbf3bd9141ba7
AntoineAssanvo <62053287+AntoineAssanvo@users.noreply.github.com>
2020-06-07 10:16:38 +0200

    changement en php

---
+++ php/transition_1.php
@@ -0,0 +1,5 @@
+<div class="transition">
+    <img src="../images/page_img/fond_middle_1.png" class="fond_mid_1" alt=""></div>
+<div class="transition_2">
+    <img src="../images/page_img/fond_middle_2.png" class="fond_mid_2" alt="">
+</div>
---
+++ php/transition_2.php
@@ -0,0 +1,7 @@
+<div class="transition">
+    <div class="btn_up">
+        <a href="#accueil">
+            <img src="../images/arrow.png" class="up_arrow up " alt="Retourner à la page précédente"></a>
+    </div>
+    <img src="../images/page_img/fond_end.png" class="fond_end " alt="">
+</div>
